23004e3ca18df44bd86e502879c5b6b06ccb1975
Barneslow <darrachb1991@gmail.com>
2022-06-01 17:32:34 +0100

    fixed order submit error

--- src/store/cart-context/CartProvider.js
+++ src/store/cart-context/CartProvider.js
@@ -8,6 +8,7 @@ const INITIAL_CART_STATE = {
 };
 
 const cartReducer = (state, action) => {
+  console.log(action);
   if (action.type === "ADD_CART_ITEM") {
     const newTotalAmount =
       state.totalAmount + action.payload.price * action.payload.amount;
@@ -38,7 +39,34 @@ const cartReducer = (state, action) => {
     };
   }
 
-  if ((action.type = "REMOVE_CART_ITEM")) {
+  // if ((action.type = "REMOVE_CART_ITEM")) {
+  //   const existingCartItemIndex = state.items.findIndex((item) => {
+  //     return item.id === action.payload;
+  //   });
+
+  //   const existingCartItem = state.items[existingCartItemIndex];
+
+  //   const newTotalAmount = state.totalAmount - existingCartItem.price;
+
+  //   let updatedItems;
+  //   if (existingCartItem.amount === 1) {
+  //     updatedItems = state.items.filter((item) => item.id !== action.payload);
+  //   } else {
+  //     const updatedItem = {
+  //       ...existingCartItem,
+  //       amount: existingCartItem.amount - 1,
+  //     };
+  //     updatedItems = [...state.items];
+  //     updatedItems[existingCartItemIndex] = updatedItem;
+  //   }
+
+  //   return {
+  //     items: updatedItems,
+  //     totalAmount: newTotalAmount,
+  //   };
+  // }
+
+  if (action.type === "REMOVE_CART_ITEM") {
     const existingCartItemIndex = state.items.findIndex((item) => {
       return item.id === action.payload;
     });
@@ -48,6 +76,7 @@ const cartReducer = (state, action) => {
     const newTotalAmount = state.totalAmount - existingCartItem.price;
 
     let updatedItems;
+
     if (existingCartItem.amount === 1) {
       updatedItems = state.items.filter((item) => item.id !== action.payload);
     } else {
@@ -66,7 +95,6 @@ const cartReducer = (state, action) => {
   }
 
   if (action.type === "CLEAR") {
-    return INITIAL_CART_STATE;
   }
 
   return INITIAL_CART_STATE;
